80f572cc57d10b0d7ca5cf5be619a2bb408c757f
t-komiya2222 <80876633+t-komiya2222@users.noreply.github.com>
2021-10-26 16:01:46 +0900

    OGP edit

--- api/app/helpers/application_helper.rb
+++ api/app/helpers/application_helper.rb
@@ -1,21 +1,21 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'Portfolio for ...',
+      site: 'WEBエンジニア転職希望者アプリ',
       title: 'Portfolio for ...',
       reverse: true,
       separator: '|',
-      description: 'ディスクリプション',
+      description: 'WEB未経験エンジニアのポートフォリオ投稿サイトです。',
       keywords: '転職者,Web',
       canonical: request.original_url,
       noindex: ! Rails.env.production?,
       icon: [
         { href: image_url('favicon.jpeg') },
-        { href: image_url('OGP.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+        { href: image_url('OGP.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
       ],
       twitter: {
         card: 'summary_large_image',
-        site: '@ツイッターのアカウント名',
+        site: '@test',
       }
     }
   end
